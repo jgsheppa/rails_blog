652c06a9e409a659a6417773aeed4baa21f9ce82
James Sheppard <jgsheppa@protonmail.com>
2024-08-02 21:39:02 +0000

    add devise and tailwind gems

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,11 +1,12 @@
 class PostController < ApplicationController
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
-
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -23,9 +24,31 @@ class PostController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(blog_post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def blog_post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "post#index"
 
+  # resources :post, as: :post
   get "/blog/new", to: "post#new", as: :new_post
   get "/blog/:id", to: "post#show", as: :post
+  patch "/blog/:id", to: "post#update"
+  delete "/blog/:id", to: "post#destroy"
+  get "/blog/:id/edit", to: "post#edit", as: :edit_post
   post "/blog/create", to: "post#create", as: :posts
 end
