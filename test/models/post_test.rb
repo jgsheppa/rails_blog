2afe51d53f9ae2cc50921964abbf05c62b925c77
James Sheppard <jgsheppa@protonmail.com>
2024-08-03 11:46:33 +0000

    add tests for scope functions

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for drafted blog post" do
+    assert posts(:draft).draft?
+  end
+
+  test "draft? returns false for a published blog post" do
+    refute posts(:published).draft?
+  end
+
+  test "draft? returns false for a scheduled blog post" do
+    refute posts(:scheduled).draft?
+  end
+
+  test "published? returns true for published blog post" do
+    assert posts(:published).published?
+  end
+
+  test "published? returns false for a drafted blog post" do
+    refute posts(:draft).published?
+  end
+
+  test "published? returns false for a scheduled blog post" do
+    refute posts(:scheduled).published?
+  end
+
+  test "scheduled? returns true for scheduled blog post" do
+    assert posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for a published blog post" do
+    refute posts(:published).scheduled?
+  end
+
+  test "scheduled? returns false for a draft blog post" do
+    refute posts(:draft).scheduled?
+  end
 end
